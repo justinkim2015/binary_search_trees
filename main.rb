d9907588d4718abbd90c62b92c6a3338ba871e4c
justinkim2015 <justinkim2015@gmail.com>
2022-05-20 12:48:45 +0900

    Finish build_tree method

--- main.rb
+++ main.rb
@@ -4,26 +4,49 @@ class Tree
   attr_accessor :root, :array
 
   def initialize(array)
-    @root = nil
-    @left = nil
-    @right = nil
-    @array = array
+    @array = array.sort.uniq
+    @root = build_tree(@array, 0, @array.length - 1)
   end
 
   def build_tree(array, start, finish)
-    # base case is wrong somehow, its stackoverflow on line 18
     return if start > finish
 
-    # set middle value to root
-    mid = (start + finish) / 2 
+    mid = (start + finish) / 2
     node = Node.new(array[mid])
-
-    # RECURSIVE
-    binding.pry
     node.left = build_tree(array[0..mid - 1], start, mid - 1)
-    node.right = build_tree(array[mid + 1..-1], start, mid - 1) # this is broken
-
+    node.right = build_tree(array[mid + 1..-1], start, mid - 1)
     node
+  end
+
+  # still broken but getting there
+  def bft 
+    return if @root.nil?
+
+    queue = []
+    array = []
+    queue << @root
+    until queue.empty?
+      current = queue[0]
+      array << current.data
+      queue.unshift current.left unless current.left.nil?
+      queue.unshift current.right unless current.right.nil?
+      queue.pop
+    end
+    array
+  end
+
+  def insert(value)
+    current = @root
+    nxt = @root
+    until current.value == value
+      current = nxt
+    end
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 end
 
@@ -37,8 +60,11 @@ class Node
   end
 end
 
-array = [1, 2, 3, 5, 6, 7, 9]
-mid = 3
-tree = Tree.new(array)
-last_index = array.length - 1 # 6
-tree.build_tree(array, 0, last_index)
+arr = [1, 2, 3, 5, 6, 7, 9]
+array = []
+rand(10).times do
+  array << rand(100)
+end
+tree = Tree.new(arr)
+tree.pretty_print
+tree.bft
